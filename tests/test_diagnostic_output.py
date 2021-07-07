de472e7a9c7ad64e89e5856d994c6903920e3c51
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-07-07 14:31:01 +0200

    renames

--- tests/test_diagnostic_output.py
+++ tests/test_diagnostic_output.py
@@ -5,54 +5,54 @@ from fmdap.diagnostic_output import DiagnosticType
 import pytest
 
 # common
-filename1 = "tests/testdata/Diagnostics_F16_EnKF.dfs0"
-filename2 = "tests/testdata/diagnostics_nonMeas_SSC1.dfs0"
-filename3 = "tests/testdata/Global_stats.dfs0"
-filename1_alti = "Diagnostics_Altimetry_C2.dfs0"
-filename1_OI = "Diagnostics_Drogden_OI.dfs0"
+filename_EnKF = "tests/testdata/Diagnostics_F16_EnKF.dfs0"
+filename_nonMeas = "tests/testdata/diagnostics_nonMeas_SSC1.dfs0"
+filename_GlobalStats = "tests/testdata/Global_stats.dfs0"
+filename_EnKF_alti = "Diagnostics_Altimetry_C2.dfs0"
+filename_OI = "Diagnostics_Drogden_OI.dfs0"
 
 
 def test_folder():
-    assert os.path.exists(filename1)
+    assert os.path.exists(filename_EnKF)
 
 
 def test_read():
-    diag = read_diagnostic(filename1)
+    diag = read_diagnostic(filename_EnKF)
     assert diag.df.values[0, 0] == 1.749464750289917
     assert diag.df.shape == (744, 12)
 
 
 def test_diag_type_1():
-    diag = read_diagnostic(filename1)
+    diag = read_diagnostic(filename_EnKF)
     assert diag.type == DiagnosticType.Measurement
 
 
-def test_diag_type_1():
-    diag = read_diagnostic(filename1)
-    assert diag.type == DiagnosticType.Measurement
+def test_diag_type_2():
+    diag = read_diagnostic(filename_nonMeas)
+    assert diag.type == DiagnosticType.NonMeasurementPoint
 
 
 def test_diag_type_3():
-    diag = read_diagnostic(filename3)
+    diag = read_diagnostic(filename_GlobalStats)
     assert diag.type == DiagnosticType.GlobalAssimilationStatistics
 
 
 def test_get_total_forecast():
-    diag = read_diagnostic(filename1)
+    diag = read_diagnostic(filename_EnKF)
     dff = diag.forecast.df
     assert dff.values[-1, 4] == 5.08772611618042
     assert dff.shape == (397, 12)
 
 
 def test_get_total_analysis():
-    diag = read_diagnostic(filename1)
+    diag = read_diagnostic(filename_EnKF)
     dfa = diag.analysis.df
     assert dfa.values[-1, 4] == 5.099072456359863
     assert dfa.shape == (347, 12)
 
 
 def test_idx_at_updates():
-    diag = read_diagnostic(filename1)
+    diag = read_diagnostic(filename_EnKF)
     iforecast = diag.idx_forecast
     nupdates = len(iforecast[iforecast == True])
     assert len(iforecast) == 744
@@ -61,7 +61,7 @@ def test_idx_at_updates():
 
 
 def test_get_iforecast_from_ianalysis():
-    diag = read_diagnostic(filename1)
+    diag = read_diagnostic(filename_EnKF)
     ianalysis = diag.idx_analysis
     iforecast2 = diag.get_iforecast_from_ianalysis(ianalysis)
 
@@ -69,19 +69,19 @@ def test_get_iforecast_from_ianalysis():
 
 
 def test_get_increments():
-    diag = read_diagnostic(filename1)
+    diag = read_diagnostic(filename_EnKF)
     dfi = diag.increment.df
     assert dfi.values[3, 3] == 0.013921260833740234
 
 
 def test_get_all_increments_as_array():
-    diag = read_diagnostic(filename1)
+    diag = read_diagnostic(filename_EnKF)
     arr = diag.increment.values.ravel()
     assert len(arr) == 3470
 
 
 def test_get_mean_increments():
-    diag = read_diagnostic(filename1)
+    diag = read_diagnostic(filename_EnKF)
     ianalysis = diag.idx_analysis
     dfi = diag._get_mean_increments()
     assert dfi.values[3, 0] == 0.03600311279296875
