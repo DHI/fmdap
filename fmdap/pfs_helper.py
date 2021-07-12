c820a253be770f4aaf0d05f9d0d270965a428280
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-07-12 13:02:46 +0200

    add get_diagnostics_df

--- fmdap/pfs_helper.py
+++ fmdap/pfs_helper.py
@@ -2,27 +2,42 @@ from collections import namedtuple
 import pandas as pd
 from mikeio import Pfs
 
+
 def pfs2dict(pfs_file):
     # with open(pfs_file, encoding='iso8859-1') as f:
     #         pfs = f.read()
-    #y = pfs2yaml(pfs)
-    #d = pfs2yaml.pfs2dict(pfs)
+    # y = pfs2yaml(pfs)
+    # d = pfs2yaml.pfs2dict(pfs)
     d = Pfs(pfs_file)._data
     return d
+
 
 def get_DA_dict(d):
     root = list(d.keys())[0]
     return d[root]["DATA_ASSIMILATION_MODULE"]
 
+
 def get_DA_sections(d):
     return list(get_DA_dict(d).keys())
 
+
 def get_measurements_df(dda):
-    #dda = get_DA_dict(d)
+    # dda = get_DA_dict(d)
     meas_sec = dda["MEASUREMENTS"]
-    n_meas = int(meas_sec['number_of_independent_measurements'])
+    n_meas = int(meas_sec["number_of_independent_measurements"])
 
     raw = {}
-    for j in range(1,n_meas+1):
-        raw[j] = meas_sec[f'MEASUREMENT_{j}']
+    for j in range(1, n_meas + 1):
+        raw[j] = meas_sec[f"MEASUREMENT_{j}"]
+    return pd.DataFrame(raw).T
+
+
+def get_diagnostics_df(dda):
+    # dda = get_DA_dict(d)
+    diag_sec = dda["DIAGNOSTICS"]["OUTPUTS"]
+    n_diag = int(diag_sec["number_of_outputs"])
+
+    raw = {}
+    for j in range(1, n_diag + 1):
+        raw[j] = diag_sec[f"OUTPUT_{j}"]
     return pd.DataFrame(raw).T
