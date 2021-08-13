51b79369cbc19fd1e7056752726c4af93dd385c5
Jesper Sandvig Mariegaard <34088801+jsmariegaard@users.noreply.github.com>
2021-08-13 10:46:24 +0200

    docstrings

--- fmdap/AR1.py
+++ fmdap/AR1.py
@@ -6,25 +6,27 @@ import pandas as pd
 
 
 def phi_to_halflife(phi, dt=1):
+    """Convert the AR(1) propagation parameter phi to half-life"""
     rho = dt / (np.log2(1 / phi))
     return rho
 
 
 def halflife_to_phi(rho, dt=1):
+    """Convert half-life to the AR(1) propagation parameter phi"""
     phi = 0.5 ** (dt / rho)
     # st_dev = st_dev * np.sqrt(1 - phi**2)
     return phi
 
 
-def fit_AR1(data):
-    # Fit an AR(1) model to the data
-
+def _fit_AR1(data):
+    """Fit AR(1) model to the data"""
     mod = ARIMA(data, order=(1, 0, 0))
     res = mod.fit()
     return res
 
 
 def estimate_AR1_halflife(df):
+    """Estimate AR(1) half-life of columns in DataFrame"""
     if not isinstance(df, (pd.DataFrame, pd.Series)):
         raise TypeError(f"Type {type(df)} not supported (DataFrame/Series)")
     data = df.to_numpy()
@@ -32,7 +34,7 @@ def estimate_AR1_halflife(df):
     if data.squeeze().ndim == 2:
         return np.array([estimate_AR1_halflife(df[col]) for col in df])
 
-    res = fit_AR1(df)
+    res = _fit_AR1(df)
     phi = res.params[1]
 
     try:
